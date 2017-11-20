95a3804ea44db726b6941695022efdd1017b02b0
matiullah karimi <matiullah.karimi@gmail.com>
2017-11-20 21:22:15 +0430

    Add UploadFile activity

--- app/src/main/java/com/example/mkarimi/icu/MainActivity.java
+++ app/src/main/java/com/example/mkarimi/icu/MainActivity.java
@@ -154,7 +154,8 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
-            // Handle the camera action
+            Intent intent = new Intent(this, UploadFile.class);
+            startActivity(intent);
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
